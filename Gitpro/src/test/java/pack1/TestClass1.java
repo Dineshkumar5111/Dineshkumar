f6e433637adc6419a51b3d57349d9470a287458e
Dineshkumar5111 <118249109+Dineshkumar5111@users.noreply.github.com>
2022-11-15 13:51:22 +0530

    Update TestClass1.java

--- Gitpro/src/test/java/pack1/TestClass1.java
+++ Gitpro/src/test/java/pack1/TestClass1.java
@@ -2,8 +2,23 @@ package pack1;
 
 public class TestClass1 {
  public static void main(String[] args) {
+	System.out.println("Dineshkumar");
 	System.out.println("TC01");
-	
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+
+
+
+
+
+
+
 
 }
 }
