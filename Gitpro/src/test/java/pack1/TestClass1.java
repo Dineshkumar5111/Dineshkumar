5f23685c4fb2292afda9e2cb89d69dddec7433b9
Dineshkumar5111 <118249109+Dineshkumar5111@users.noreply.github.com>
2022-11-15 13:56:26 +0530

    Update TestClass1.java

--- Gitpro/src/test/java/pack1/TestClass1.java
+++ Gitpro/src/test/java/pack1/TestClass1.java
@@ -3,15 +3,15 @@ package pack1;
 public class TestClass1 {
  public static void main(String[] args) {
 	System.out.println("Dineshkumar");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
+	System.out.println("Vikas");
+	System.out.println("Nikam");
+	System.out.println("M.sc.B.ed Chemistry");
+	System.out.println("AT.Post.Palaskhede seem");
+	System.out.println("Tal.parola");
+	System.out.println("Dist.Jalgaon");
+	System.out.println("Maharshtra");
+	System.out.println("India");
+	System.out.println("Shree Swami Samarth");
 
 
 
